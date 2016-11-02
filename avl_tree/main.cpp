5b1941257a14cd3b94f30c5c4bf5509cea23815b
Zhijiang TAO <taozhijiang@gmail.com>
2016-11-02 12:20:20 +0800

    avl tree

---
+++ avl_tree/main.cpp
@@ -0,0 +1,27 @@
+#include "avl_tree.hpp"
+#include <assert.h>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "AVL Tree Test" << std::endl;
+
+    AvlTree avltree;
+    avltree.root_ = avltree.insert(avltree.root_, 8);
+    avltree.root_ = avltree.insert(avltree.root_, 5);
+    avltree.root_ = avltree.insert(avltree.root_, 4);
+    avltree.root_ = avltree.insert(avltree.root_, 11);
+    avltree.root_ = avltree.insert(avltree.root_, 15);
+    avltree.root_ = avltree.insert(avltree.root_, 3);
+    avltree.root_ = avltree.insert(avltree.root_, 6); 
+    avltree.root_ = avltree.insert(avltree.root_, 2);
+
+    avltree.display(avltree.root_, 1);
+
+    avltree.root_ = avltree.remove(avltree.root_, 3);
+    avltree.root_ = avltree.remove(avltree.root_, 8);
+
+    avltree.display(avltree.root_, 1);
+
+    return 0;
+}
